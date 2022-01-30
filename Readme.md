7f64e277c631d80380d51b50ba7e1fb548c2b965
Krish Sethi <95207265+Krish-198@users.noreply.github.com>
2022-01-30 14:54:36 +0530

    First commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Python Calculater
+
+**This program calculates Anything.
+This Calculater was made using python.
+This software uses Time extension.
+Download the EXE file and run it to get your answer.**
+
+# How to use?
+* Download the Zip file.
+* Find the Exe File.
+* Run the exe File
+* Then Caclculate whatever you want.
+
+## Edit This project
+* Download the Zip file.
+* Find the .py File.
+* Then use your python editor to edit it.
---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+
+
+
+print ("Addition Calculator") 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print (sum)
+print ("Minus Calculator") 
+num3 = int(input("Enter first number: "))
+num4 = int(input("Enter second number: "))
+ans = num3 - num4
+print (ans)
+print ("Multiplication Calculator") 
+num5 = int(input("Enter first number: "))
+num6 = int(input("Enter second number: "))
+ans2 = num5 * num6
+print (ans2)
+print ("Division Calculator")
+num7 = int(input("Enter first number: "))
+num8 = int(input("Enter second number: "))
+ans3 = num7 / num8
+print (ans3)
+time.sleep(60)
